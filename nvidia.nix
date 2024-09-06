e5d39a2f48f3dfe6cd94cdb4871b47bf262370b1
Sean Missingham <sean@missingham.net>
2024-09-06 11:38:35 -0500

    29 current  2024-09-06 11:38:33  24.05.4469.6e99f2a27d60  6.6.48                          *

--- nvidia.nix
+++ nvidia.nix
@@ -45,7 +45,7 @@
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     #package = config.boot.kernelPackages.nvidiaPackages.stable;
     #package = config.boot.kernelPackages.nvidiaPackages.beta; # accentuates flickering issue
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    #package = config.boot.kernelPackages.nvidiaPackages.production;
 
     ## CUSTOM DRIVER VERSIONS IN TESTING ##
 
@@ -74,12 +74,12 @@
     #};
 
     # Runs Factorio, firefox crashing
-    # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    #   version = "555.58.02";
-    #   sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
-    #   settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
-    #   persistencedSha256 = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
-    # };
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "555.58.02";
+      sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+      settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+      persistencedSha256 = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+    };
 
     # Stable games, firefox crashing
     # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
