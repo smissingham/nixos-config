74b681634cc53f4f1850236ea07fb7786e9bea78
Sean Missingham <sean@missingham.net>
2024-09-08 09:32:32 -0500

    42 current  2024-09-08 09:29:53  24.05.4469.6e99f2a27d60  6.6.48                               *

--- nvidia.nix
+++ nvidia.nix
@@ -18,8 +18,8 @@
   # Enable OpenGL
   hardware.opengl = {
     enable = true;
-    #driSupport = true;
-    #driSupport32Bit = true;
+    driSupport = true;
+    driSupport32Bit = true;
   };
 
   # Load nvidia driver for Xorg and Wayland
