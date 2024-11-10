8db2fee8ecb5a052ac04de9a43c053e83a70157a
Sean Missingham <sean@missingham.net>
2024-11-10 17:06:08 -0600

    69 current  2024-11-05 09:26:37  24.05.4469.6e99f2a27d60  6.6.48                               *

--- docker-services/gameservers/factorio/temp/currently-playing/control.lua
+++
@@ -1,8 +0,0 @@
-local handler = require("event_handler")
-handler.add_lib(require("freeplay"))
-
-if script.active_mods["space-age"] then
-  handler.add_lib(require("space-finish-script"))
-else
-  handler.add_lib(require("silo-script"))
-end
