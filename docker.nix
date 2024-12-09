ebdd5354f4dea8e1556b17f20fcef22231ea41c1
Sean Missingham <sean@missingham.net>
2024-12-09 11:20:09 -0600

    93 current  2024-12-09 11:20:05  24.11.710650.e2605d0744c2  6.6.63                               *

--- docker.nix
+++ docker.nix
@@ -9,11 +9,10 @@
 
   hardware.nvidia-container-toolkit.enable = true;
 
-  # currently broken after 24.11, see https://github.com/NVIDIA/nvidia-container-toolkit/issues/434
-  #virtualisation.docker.rootless = {
-  #  enable = true;
-  #  setSocketVariable = true;
-  #};
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
 
   environment.systemPackages = with pkgs; [
     docker-compose
