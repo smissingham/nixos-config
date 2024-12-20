a5e0d94e730e2078362d9d0a5270302cef16d0e2
Sean Missingham <sean@missingham.com>
2024-12-20 08:19:32 -0600

    21 current  2024-12-20 08:19:28  24.11.711349.394571358ce8  6.6.66                          *

--- configuration.nix
+++ configuration.nix
@@ -53,7 +53,14 @@
     htop
     pciutils
     v4l-utils
-    wezterm
+
+    (import
+      (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/957d95fc8b9bf1eb60d43f8d2eba352b71bbf2be.tar.gz";
+        sha256 = "sha256:0jkxg1absqsdd1qq4jy70ccx4hia3ix891a59as95wacnsirffsk";
+      })
+      {inherit system;})
+    .wezterm
 
     # NIX SPECIFICS
     alejandra # nix file formatter
