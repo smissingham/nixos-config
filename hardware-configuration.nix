8e26c691d2455440f0fb5cf7595b5335fdcc4f0b
Sean Missingham <sean@missingham.com>
2024-12-20 07:55:18 -0600

    16 current  2024-12-20 07:53:01  24.11.711349.394571358ce8  6.6.66                          *

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -116,6 +116,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   # #### WEBCAM CONFIG ####
