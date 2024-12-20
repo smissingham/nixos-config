0e75c5f3e5921345442e37bd872cc8cf06032bf4
Sean Missingham <sean@missingham.com>
2024-12-20 10:03:25 -0600

    29 current  2024-12-20 10:03:21  24.11.711349.394571358ce8  6.6.66                          *

--- user-settings.nix
+++ user-settings.nix
@@ -72,6 +72,25 @@ in
         };
 
         programs.zsh = {
+          enableCompletion = true;
+          #enableAutosuggestions = true;
+          #syntaxHighlighting.enable = true;
+
+          shellAliases = {
+            ll = "ls -l";
+            update = "sudo nixos-rebuild switch";
+          };
+
+          #history = {
+          #  size = 10000;
+          #  path = "${config.xdg.dataHome}/zsh/history";
+          #};
+
+          oh-my-zsh = {
+            enable = true;
+            plugins = ["git" "thefuck"];
+            theme = "robbyrussell";
+          };
         };
 
         imports = [
