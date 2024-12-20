d01924f83c5c35f6e00df562580ecd41c09bb6b1
Sean Missingham <sean@missingham.com>
2024-12-20 07:25:56 -0600

    9 current   2024-12-20 07:25:54  24.11.711349.394571358ce8  6.6.66                          *

--- user-settings.nix
+++ user-settings.nix
@@ -76,6 +76,12 @@ in
 
         programs.wezterm = {
           enable = true;
+          extraConfig = ''
+            return {
+              enable_wayland = true,
+              front_end = "OpenGL",
+            }
+          '';
         };
 
         imports = [
