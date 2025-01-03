531af98ae2b65139b5d18292da58a2fb8f4356d1
Sean Missingham <sean@missingham.com>
2025-01-03 16:17:48 -0600

    109 current  2025-01-03 16:17:35  24.11.711349.394571358ce8  6.6.66                          *

--- network.nix
+++ network.nix
@@ -9,7 +9,11 @@
 
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [8080 8443];
+    allowedTCPPorts = [
+      3389
+      8080
+      8443
+    ];
     allowedUDPPortRanges = [
       #{ from = 4000; to = 4007; }
       #{ from = 8000; to = 8010; }
